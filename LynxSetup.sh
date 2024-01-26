4e821c7ad3c61636b6ad01688be1e4dbd22b9238
SwarJagdale <128358541+SwarJagdale@users.noreply.github.com>
2024-01-26 21:05:27 +0530

    Update LynxSetup.sh

--- LynxSetup.sh
+++ LynxSetup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 REPO_URL="https://github.com/SwarJagdale/LynxCLI.git"
-#Directory where lynxCLI will be saved
+# Directory where LynxCLI will be saved
 REPO_DIR="$HOME/LynxCLI"
 
+# Clone the GitHub repository
 git clone "$REPO_URL" "$REPO_DIR"
 
 # Check if the repository was cloned successfully
 if [ $? -eq 0 ]; then
     echo "Repository cloned successfully."
     
+    # Make all shell scripts executable within the repository directory
+    find "$REPO_DIR" -type f -name "*.sh" -exec chmod +x {} \;
+
     # Create an alias for running ./lynx.sh as 'lynxcli'
     echo "alias lynxcli='$REPO_DIR/lynx.sh'" >> ~/.bashrc
 
